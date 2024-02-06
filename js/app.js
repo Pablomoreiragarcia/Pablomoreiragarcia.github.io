43322d95b411c6400de703726898e22215c1c561
Pablo Moreira Gracia <Pablomoreiragarcia@gmail.com>
2024-02-06 02:01:04 +0100

    Version1-Not Finished
    Home, Contact and Footage done, Bar must have changes

---
+++ js/app.js
@@ -0,0 +1,113 @@
+particlesJS(
+    {
+    "particles": {
+      "number": {
+        "value": 150,
+        "density": {
+          "enable": true,
+          "value_area": 500
+        }
+      },
+      "color": {
+        "value": "#67dcff"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 1,
+          "color": "#5ef7ff"
+        },
+        "polygon": {
+          "nb_sides": 8
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.6459235919653404,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 6.062394805484869,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 100,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 126.03387160299326,
+        "color": "#67dcff",
+        "opacity": 0.4,
+        "width": 5
+      },
+      "move": {
+        "enable": true,
+        "speed": 5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+  
+  );
