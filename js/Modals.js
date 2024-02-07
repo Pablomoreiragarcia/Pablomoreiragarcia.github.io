6e75e81f0256f8074849fb2ae486a25aa1047e7c
Pablo Moreira Garcia <Pablomoreiragarcia@gmail.com>
2024-02-07 19:55:30 +0100

    Version3-Not Finished
    Finished Curriculum section, have to modify navbar, contact section and make portfolio section

--- js/Modals.js
+++ js/Modals.js
@@ -1,11 +1,28 @@
-const btnAbrirModal = document.querySelector("#openModalBtn");
-const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
-const modal = document.querySelector("#modal");
+const btnAbrirModal = document.querySelectorAll(".openModalBtn");
+const btnCerrarModal = document.querySelectorAll(".closeModalBtn");
 
-btnAbrirModal.addEventListener("click",()=>{
-    modal.showModal();
-})
 
-btnCerrarModal.addEventListener("click",()=>{
-    modal.close();
-})
+btnAbrirModal.forEach(btn => {
+    btn.addEventListener("click", event => {
+        const li = btn.closest(".row_curriculum_item");
+        const a = li.querySelector("a");
+        const href = a.getAttribute("href");
+
+        if (href == "#Studies") {
+            document.querySelector("#modalStudies").showModal();
+        } else if (href == "#Skills") {
+            document.querySelector("#modalSkills").showModal();
+        } else if (href == "#Experience") {
+            document.querySelector("#modalExperience").showModal();
+        }
+    });
+});
+
+btnCerrarModal.forEach(btn => {
+    btn.addEventListener("click", event => {
+        const modalId = btn.parentElement.id;
+        const modal = document.getElementById(modalId); // Encuentra el modal utilizando su ID
+        modal.close(); // Cierra el modal
+        
+    });
+});
