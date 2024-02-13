4726bcd39e6f9b353ee524f8780a3d759deef197
Pablo Moreira Gracia <Pablomoreiragarcia@gmail.com>
2024-02-13 03:00:03 +0100

    Version7-Not Finished
    Small changes.

--- js/navBar.js
+++ js/navBar.js
@@ -1,35 +1,35 @@
-!function(a) {
+!function(a){
     "use strict";
-    a('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-            var o = a(this.hash);
-            if ((o = o.length ? o : a("[name=" + this.hash.slice(1) + "]")).length)
+    a('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(){
+        if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){
+            var o=a(this.hash);
+            if((o=o.length?o:a("[name="+this.hash.slice(1)+"]")).length)
                 return a("html, body").animate({
-                    scrollTop: o.offset().top - 54
-                }, 60, "easeInOutExpo"),
+                    scrollTop:o.offset().top-54
+                },30,"easeInOutExpo"),
                 !1
         }
-    }),
-
-
-    a(".js-scroll-trigger").click(function() {
-        a(".navbar-collapse").collapse("hide")
-    }),
-
-
-    new bootstrap.ScrollSpy(document.body, {
-        target: '#navbar-example'
-      });
-
-    var o = function() {
-        a("#mainNav").offset().top > 100 ? a("#mainNav").addClass("navbar-shrink") : a("#mainNav").removeClass("navbar-shrink")
+    });
+    var o=function(){
+        a("#mainNav").offset().top>100?a("#mainNav").addClass("navbar-shrink"):a("#mainNav").removeClass("navbar-shrink")
     };
     o(),
-    a(window).scroll(o),
-    a(".portfolio-modal").on("show.bs.modal", function(o) {
+    a(window).scroll(o)/*,
+
+
+    a(".js-scroll-trigger").click(function(){
+        a(".navbar-collapse").collapse("hide")
+    }),
+    a("body").scrollspy({
+        target:"#mainNav",offset:56
+    });
+    a(".portfolio-modal").on("show.bs.modal",function(o){
         a(".navbar").addClass("d-none")
     }),
-    a(".portfolio-modal").on("hidden.bs.modal", function(o) {
+    a(".portfolio-modal").on("hidden.bs.modal",function(o){
         a(".navbar").removeClass("d-none")
     })
+*/
 }(jQuery);
+
+
