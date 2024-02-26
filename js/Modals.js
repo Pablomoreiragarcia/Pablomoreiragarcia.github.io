d8f0f71194909c838c6114021ad64623b9864686
Pablo Moreira Garcia <61966322+Pablomoreiragarcia@users.noreply.github.com>
2024-02-26 13:37:59 +0100

    Version9-Finished
    Added some projects

--- js/Modals.js
+++ js/Modals.js
@@ -4,25 +4,21 @@ const btnCerrarModal = document.querySelectorAll(".closeModalBtn");
 
 btnAbrirModal.forEach(btn => {
     btn.addEventListener("click", event => {
-        const li = btn.closest(".row_curriculum_item");
+        const li = btn.closest(".modal_item");
         const a = li.querySelector("a");
         const href = a.getAttribute("href");
-
-        if (href == "#Studies") {
-            document.querySelector("#modalStudies").showModal();
-        } else if (href == "#Skills") {
-            document.querySelector("#modalSkills").showModal();
-        } else if (href == "#Experience") {
-            document.querySelector("#modalExperience").showModal();
+        
+        if (href) {
+            document.querySelector("#modal" + href.substring(1)).showModal();
         }
     });
 });
 
 btnCerrarModal.forEach(btn => {
     btn.addEventListener("click", event => {
-        const modalId = btn.parentElement.id;
-        const modal = document.getElementById(modalId); // Encuentra el modal utilizando su ID
-        modal.close(); // Cierra el modal
+        const modalId = btn.parentElement.parentElement.id;
+        const modal = document.getElementById(modalId);
+        modal.close();
         
     });
 });
